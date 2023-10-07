f59c873d3dea13984b32683e4e1044824611725f
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-08 03:37:19 +0530

    reports completed

--- controllers/report_controller.js
+++ controllers/report_controller.js
@@ -1,3 +1,16 @@
-module.exports.reports = function(req, res){
-  
-}
+const Doctor = require("../models/doctor");
+const Patient = require("../models/patient");
+const Report = require("../models/report");
+
+module.exports.report = async function (req, res) {
+  try {
+    const status = req.params.status;
+    console.log(status);
+    let report = await Report.find({ status: status })
+      .populate("doctor", "name")
+      .populate("patient", "name");
+    res.status(200).send(report);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
--- routes/report_router.js
+++ routes/report_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const reportController = require('../controllers/report_controller');
 
-router.get('/:status', reportController.reports);
+router.get('/:status', reportController.report);
 
 module.exports = router;
 
