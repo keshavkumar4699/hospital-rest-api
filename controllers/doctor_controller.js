f41ae4b47d19b4d204584fc166415f1e7c6c21df
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-07 11:21:23 +0530

    doctor created

--- controllers/doctor_controller.js
+++ controllers/doctor_controller.js
@@ -1,7 +1,25 @@
-module.exports.register = function(req, res){
+const Doctor = require("../models/doctor");
 
-}
+module.exports.register = async function (req, res) {
+  try {
+    //find all doctors with given phone or email
+    let doctors = await Doctor.find({
+      $or: [{ phone: req.body.phone }, { email: req.body.email }],
+    });
 
-module.exports.login = function(req, res){
-  
-}
+    //create doctor if it doesn't exist
+    if (doctors.length != 0) {
+      res.status(409).send("Doctor exists!");
+    } else {
+      await Doctor.create(req.body);
+      res.status(200).send("Doctor created!");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports.login = function (req, res) {
+  console.log(req.query.id);
+  console.log(req.params.id);
+};
